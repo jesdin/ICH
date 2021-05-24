61c07dc2ba1f4284e5302b06c4bcfc75cfa55f6a
Jesdin Raphael <jesdinraphael@gmail.com>
2021-05-24 14:16:15 +0530

    call mkdir and clear task on start
    on server start
    1. create tmp directory if not exists
    2. start scheduler to clear files not accessed within last 60 min

--- main/apps.py
+++ main/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
-
+import os
+from main.clear_task import clear_temp
 
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
+    def ready(self):
+        if not os.path.exists("assets\\tmp"):
+            os.makedirs("assets\\tmp")
+        clear_temp()
--- main/clear_task.py
+++ main/clear_task.py
@@ -1,5 +1,27 @@
 import threading
+import os, stat, time
 
-def printit():
-  threading.Timer(5.0, printit).start()
-  print("Hello, World!")
+PATH = "assets/tmp/"
+
+def clear_temp():
+  # get list of files in tmp directory
+  f = []
+  for root, dirs, files in os.walk(PATH):
+  	for file in files:
+  		f.append(os.path.join(root,file))  
+
+  # get local time
+  localTime = time.mktime(time.localtime())
+  for file in f:
+    # get access time of file
+    accessTime = time.ctime ( os.stat(file) [ stat.ST_ATIME ] )  
+    accessTime = time.strptime(accessTime)
+
+    # get time dif in minutes
+    dif = (localTime - time.mktime(accessTime))/60
+
+    # if last accessed is more than 60 mins del file
+    if (dif > 60):
+      os.remove(file)
+      
+  threading.Timer(3600.0, clear_temp).start()
